2d1c8459390eb447a4ffa94e5628f3932645929a
Bichngocc07 <ngoc01072006@gmail.com>
2025-06-28 11:32:15 +0700

    Update sesex06.c

--- Sess06/sesex06.c
+++ Sess06/sesex06.c
@@ -2,6 +2,21 @@
 // Created by Owner on 6/27/2025.
 //
 #include <stdio.h>
-int main() {
 
+
+int countWays(int n) {
+    if (n == 0 || n == 1) {
+        return 1;  
+    }
+    return countWays(n - 1) + countWays(n - 2);
+}
+
+int main() {
+    int steps;
+    printf("Nhập số bậc của cầu thang: ");
+    scanf("%d", &steps);
+
+    int result = countWays(steps);
+    printf("Số cách leo cầu thang là: %d\n", result);
+    return 0;
 }
